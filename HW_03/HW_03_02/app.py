1e2071c82659644c1ee36a7c039b40b24020a894
ffe4el <codkan20@gmail.com>
2024-09-20 23:36:26 +0900

    clean

--- HW_03/HW_03_02/app.py
+++ HW_03/HW_03_02/app.py
@@ -3,24 +3,22 @@ import requests
 
 app = Flask(__name__)
 
-# Your API Key
+# 공공데이터 포탈 api key
 API_KEY = "TEo5fURVg6O3ChvfXOmzkr0IXbTl0d4VkfIj3JVTz0ctJ+NS0IjPHxLXlijxDlubeXvzd3ZlGksTn/HhACp8gA=="
 
-# Route to serve the HTML page
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# Flask route to handle requests for farm data
+
 @app.route('/get_farm_data', methods=['GET'])
 def get_farm_data():
     farm_code = request.args.get('farm_code')
 
-    # Construct the URL for the API request
-    url = "https://apis.data.go.kr/1390000/SmartFarmdata/envdatarqst"  # 엔드포인트 확인 필요
+    url = "https://apis.data.go.kr/1390000/SmartFarmdata/envdatarqst"  # 엔드포인트 확인
     params = {
         "serviceKey": API_KEY,
-        # "pageNo": 1,
         "pageSize": 100,
         "searchFrmhsCode": farm_code,
         "returnType": "json"
@@ -28,6 +26,7 @@ def get_farm_data():
 
     response = requests.get(url, params=params)
 
+    # 에러처리 추가
     if response.status_code == 200:
         try:
             data = response.json()
@@ -36,6 +35,7 @@ def get_farm_data():
             return jsonify({"error": "Invalid JSON response", "response": response.text})
     else:
         return jsonify({"error": "Failed to fetch data"}), response.status_code
+
 
 if __name__ == "__main__":
     app.run(debug=True)
